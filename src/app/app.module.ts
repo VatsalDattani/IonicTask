57f35f78584697f1a9ecd49675430db318922b30
Vatsal Dattani <29160800+VatsalDattani@users.noreply.github.com>
2020-01-27 18:58:36 +0530

    Error with modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -19,6 +19,7 @@ import { ModalComponent } from './modal/modal.component';
     SplashScreen,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,17 @@
+
 import { Component } from '@angular/core';
+import {ModalComponent} from '../modal/modal.component';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  constructor(public modalController: ModalController) {}
 
   itemCart = {
     samosa: 0,
@@ -24,6 +28,13 @@ export class HomePage {
   removeItem = (item) => {
     if (!this.itemCart[item]) { return; }
     this.itemCart[item]--;
+  }
+
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: ModalComponent
+    });
+    return await modal.present();
   }
 
 }
